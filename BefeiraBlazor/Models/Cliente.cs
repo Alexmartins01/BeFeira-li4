e481d0bb7580c4a162b4166633e1058381897823
Hugo Martins <73354496+hugomartins22@users.noreply.github.com>
2022-12-29 19:41:29 +0000

    Add files via upload

---
+++ BefeiraBlazor/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ;
+
+public class Cliente
+{
+    public int ClienteId { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string? Email { get; set; }
+}
---
+++ BefeiraBlazor/Models/Feira.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ;
+
+public class Feira
+{
+    [ForeignKey("Categoria")]
+    public int FeiraId { get; set; }
+    public int Categoria { get; set; }
+
+}
