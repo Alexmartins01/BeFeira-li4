c8b339126f587bac906826e38f9ff280af01c69a
Alexmartins01 <61992862+Alexmartins01@users.noreply.github.com>
2022-12-29 19:33:46 +0000

    tables

---
+++ BefeiraBlazor/Models/CarrinhoProduto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BefeiraBlazor.Models
+{
+
+
+    public class CarrinhoProduto
+    {
+        [ForeignKey("CarrinhoId")]
+        public int CarrinhoId { get; set; }
+        [ForeignKey("ProdutoId")]
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+        public float Preco { get; set; }
+        public int TaxaBefeira { get; set; }
+    }
+}
