4b4c272290f4b0c858b6673f860476db0c4cc01e
Eduardo Rocha <73354143+Eduard0Rocha@users.noreply.github.com>
2023-01-11 08:27:06 +0000

    Add files via upload

---
+++ BeFeira/Client/Program.cs
@@ -0,0 +1,19 @@
+global using BeFeira.Services.StandServices;
+global using BeFeira.Client.Services.FeiraServices;
+global using BeFeira.Shared;
+using BeFeira.Client;
+using BeFeira.Client.Services.ProdutoServices;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IStandService,StandService>();
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
+builder.Services.AddScoped<IFeiraService, FeiraService>();
+
+
+await builder.Build().RunAsync();
