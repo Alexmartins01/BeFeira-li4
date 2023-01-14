e817943bdd4bd08a11bdb2d3d86cf99b0337ee1a
Hugo Martins <hugomartinstop@gmail.com>
2023-01-14 21:26:40 +0000

    search by name done

--- BeFeira/Client/Services/ProdutoServices/ProdutoService.cs
+++ BeFeira/Client/Services/ProdutoServices/ProdutoService.cs
@@ -9,28 +9,29 @@ namespace BeFeira.Client.Services.ProdutoServices
         private readonly HttpClient _http;
         private readonly NavigationManager navigationManager;
 
-        public ProdutoService(HttpClient http,NavigationManager navigation) { 
-            _http=http;
+        public ProdutoService(HttpClient http, NavigationManager navigation)
+        {
+            _http = http;
             this.navigationManager = navigation;
 
         }
-        public  List<Produto> produtos { get ; set ; }
-        
-     
-        
+        public List<Produto> produtos { get; set; }
+
+
+
 
         public async void SetProdutos(HttpResponseMessage result)
         {
             var response = await result.Content.ReadFromJsonAsync<List<Produto>>();
-            this.produtos= response;
+            this.produtos = response;
             navigationManager.NavigateTo("produto");
         }
 
 
         public async Task<List<Produto>> GetProdutosBySubcat(int idsubcat)
         {
-            List<Produto> prods= new List<Produto> ();
-            foreach(var p in produtos)
+            List<Produto> prods = new List<Produto>();
+            foreach (var p in produtos)
             {
                 if (p.SubCategoriaID == idsubcat)
                 {
@@ -41,7 +42,7 @@ namespace BeFeira.Client.Services.ProdutoServices
         }
 
 
-        public  async Task GetProdutos()
+        public async Task GetProdutos()
         {
             var result = await _http.GetFromJsonAsync<List<Produto>>("api/Produto");
             if (result != null)
@@ -55,7 +56,7 @@ namespace BeFeira.Client.Services.ProdutoServices
             var result = await _http.GetFromJsonAsync<Produto>($"api/Produto/{id}");
             if (result != null)
             {
-            
+
                 return result;
             }
             throw new Exception("Product not found");
@@ -68,7 +69,7 @@ namespace BeFeira.Client.Services.ProdutoServices
             SetProdutos(result);
         }
 
-        public async Task DeleteProduto( int id)
+        public async Task DeleteProduto(int id)
         {
 
             var result = await _http.DeleteAsync($"api/Produto/{id}");
@@ -88,13 +89,13 @@ namespace BeFeira.Client.Services.ProdutoServices
             throw new NotImplementedException();
         }
 
-         public async Task<List<Produto>> GetProdutosBySubcat4(int idsubcat)
+        public async Task<List<Produto>> GetProdutosBySubcat4(int idsubcat)
         {
-    
+
             List<Produto> prods = new List<Produto>();
             foreach (var p in produtos)
             {
-                if (p.SubCategoriaID == idsubcat && prods.Count<4)
+                if (p.SubCategoriaID == idsubcat && prods.Count < 4)
                 {
                     prods.Add(p);
                 }
@@ -102,17 +103,31 @@ namespace BeFeira.Client.Services.ProdutoServices
             return prods;
         }
 
-		public async Task<List<Produto>> GetProdutosBySeller(int idSeller)
-		{
+        public async Task<List<Produto>> GetProdutosBySeller(int idSeller)
+        {
+            List<Produto> prods = new List<Produto>();
+            foreach (var p in produtos)
+            {
+                if (p.Stand.VendedorID == idSeller)
+                {
+                    prods.Add(p);
+                }
+            }
+            return prods;
+        }
+
+
+        public async Task<List<Produto>> GetProdutosBySearch(string searchProdName)
+        {
 			List<Produto> prods = new List<Produto>();
-			foreach (var p in produtos)
-			{
-				if (p.Stand.VendedorID == idSeller)
-				{
-					prods.Add(p);
-				}
-			}
+			foreach (Produto prod in produtos)
+            {
+                if (prod.Nome_Produto.Equals(searchProdName))
+                {
+					prods.Add(prod);
+                }
+            }
 			return prods;
 		}
-	}
+    }
 }
