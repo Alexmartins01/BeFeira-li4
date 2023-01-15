7fede2d4459c5f2f6475a4bb3ffef6ac24bd54b8
Alexmartins01 <61992862+Alexmartins01@users.noreply.github.com>
2023-01-15 17:05:19 +0000

    venda produto services

---
+++ BeFeira/Client/Services/VendaProdServices/IVendaProdService.cs
@@ -0,0 +1,12 @@
+﻿namespace BeFeira.Client.Services.VendaProdServices
+{
+    public interface IVendaProdService
+    {
+        List<VendaProduto> Vendas { get; set; }
+        Task GetVendaProdutos();
+
+        Task<Venda> GetSingleVendaProduto(int idvenda);
+
+        Task AddVendaProduto(Venda venda);
+    }
+}
---
+++ BeFeira/Client/Services/VendaProdServices/VendaProdService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using BeFeira.Shared;
+
+namespace BeFeira.Client.Services.VendaProdServices
+{
+    public class VendaProdService : IVendaProdService
+    {
+        private readonly HttpClient _http;
+
+        public VendaProdService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<VendaProduto> Vendas { get; set; }
+
+
+        public async Task GetVendaProdutos()
+        {
+            var result = await _http.GetFromJsonAsync<List<VendaProduto>>("api/vendaproduto");
+            if (result != null) { Vendas = result; }
+        }
+
+        public async Task<VendaProduto> GetSingleVendaProduto(int idvenda)
+        {
+            var result = await _http.GetFromJsonAsync<VendaProduto>($"api/vendaproduto/{idvenda}");
+            if (result != null)
+            {
+                return result;
+            }
+            throw new Exception("VendaProduto not found");
+        }
+
+        public async Task AddVendaProduto(VendaProduto venda)
+        {
+            var result = await _http.PostAsJsonAsync("api/vendaproduto", venda);
+            var response = await result.Content.ReadFromJsonAsync<List<VendaProduto>>();
+            Vendas = response;
+        }
+    }
+}
